0191c31df9baad7066d2f1cbaa9262afac83c1f2
Razeev-stha <yuvrajstha1246@outlook.com>
2020-06-25 14:03:52 +0545

    Updates

---
+++ precache-manifest.424c59917f506574c4fe81364f6f6208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38b8eaa2b2d8ff98fcc11cfa76f3820",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "035ad7329dc422390864",
+    "url": "/monsters/static/css/main.12a4f8b7.chunk.css"
+  },
+  {
+    "revision": "82d3ac6fab6e3037111c",
+    "url": "/monsters/static/js/2.bc827ca8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.bc827ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035ad7329dc422390864",
+    "url": "/monsters/static/js/main.5400be38.chunk.js"
+  },
+  {
+    "revision": "e9bb85174518dbc3ccec",
+    "url": "/monsters/static/js/runtime-main.eb5e01cc.js"
+  }
+]);
